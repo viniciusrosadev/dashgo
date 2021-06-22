5bde47b3abc56e0bdff491ed69a21e069b6e109c
Vinícius Rosa <viniciusrosa3525@gmail.com>
2021-06-21 21:11:44 -0300

    Fix header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ export function Header() {
                 </Text>
             </Text>
 
-            <Flex as="label" flex='1' py="4" maxWidth={400} alignSelf="center" color="gray.200" position="relative" bg="bg.800" borderRadius="full">
+            <Flex as="label" flex='1' py="4" px="8" ml="6" maxWidth={400} alignSelf="center" color="gray.200" position="relative" bg="gray.800" borderRadius="full">
                 <Input color="gray.50" variant="unstyled" px="4" mr="4" placeholder="Buscar na plataforma" _placeholder={{ color: 'gray.400' }} />
 
                 <Icon as={RiSearchLine} fontSize="20" />
